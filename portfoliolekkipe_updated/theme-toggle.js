6d100b943d308efca954a51a0965e52f262ad681
Aylki <111666341+Aylki@users.noreply.github.com>
2023-12-20 01:40:18 +0100

    Update theme-toggle.js

--- portfoliolekkipe_updated/theme-toggle.js
+++ portfoliolekkipe_updated/theme-toggle.js
@@ -1,28 +1,28 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const themeToggleButton = document.getElementById('theme-toggle');
+    const logoImage = document.getElementById('logo-image'); // L'élément img avec l'ID 'logo-image'
+    const lightThemeLogoSrc = 'LEKKIPE titre.png'; // Le logo pour le thème clair
+    const darkThemeLogoSrc = 'LEKKIPE titre dark.png'; // Le logo pour le thème sombre (changez ceci selon votre fichier)
+
     const currentTheme = localStorage.getItem('theme');
 
-    // Apply or remove the dark theme class based on the saved preference
-    const applyDarkTheme = (apply) => {
-        const method = apply ? 'add' : 'remove';
-        document.body.classList[method]('dark-theme');
-        document.querySelector('header').classList[method]('dark-theme');
-        document.querySelector('main').classList[method]('dark-theme');
+    const applyDarkTheme = (isDark) => {
+        document.body.classList.toggle('dark-theme', isDark);
+        document.querySelector('header').classList.toggle('dark-theme', isDark);
+        document.querySelector('main').classList.toggle('dark-theme', isDark);
         document.querySelectorAll('article').forEach(article => {
-            article.classList[method]('dark-theme');
+            article.classList.toggle('dark-theme', isDark);
         });
+        logoImage.src = isDark ? darkThemeLogoSrc : lightThemeLogoSrc;
     };
 
-    // If there's a theme set in localStorage, apply it
-    if (currentTheme) {
-        applyDarkTheme(currentTheme === 'dark-theme');
+    if (currentTheme === 'dark-theme') {
+        applyDarkTheme(true);
     }
 
     themeToggleButton.addEventListener('click', () => {
         const isDarkThemeApplied = document.body.classList.contains('dark-theme');
         applyDarkTheme(!isDarkThemeApplied);
-
-        const theme = isDarkThemeApplied ? 'light' : 'dark-theme';
-        localStorage.setItem('theme', theme);
+        localStorage.setItem('theme', !isDarkThemeApplied ? 'dark-theme' : 'light');
     });
 });
