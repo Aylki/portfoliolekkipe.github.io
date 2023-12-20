62965a4ff23d347e6b2383847f5e17427b98c176
Aylki <111666341+Aylki@users.noreply.github.com>
2023-12-20 01:03:53 +0100

    Update theme-toggle.js

--- portfoliolekkipe_updated/theme-toggle.js
+++ portfoliolekkipe_updated/theme-toggle.js
@@ -1,6 +1,16 @@
-
 document.addEventListener('DOMContentLoaded', (event) => {
     const themeToggleButton = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        document.querySelector('header').classList.add(currentTheme);
+        document.querySelector('main').classList.add(currentTheme);
+        document.querySelectorAll('article').forEach(article => {
+            article.classList.add(currentTheme);
+        });
+    }
+
     themeToggleButton.addEventListener('click', () => {
         document.body.classList.toggle('dark-theme');
         document.querySelector('header').classList.toggle('dark-theme');
@@ -8,5 +18,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
         document.querySelectorAll('article').forEach(article => {
             article.classList.toggle('dark-theme');
         });
+
+        let theme = document.body.classList.contains('dark-theme') ? 'dark-theme' : 'light';
+        localStorage.setItem('theme', theme);
     });
 });
