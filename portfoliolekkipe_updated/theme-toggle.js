0d6fa74219135f439a51e18835acaaaccb5ecee0
Aylki <111666341+Aylki@users.noreply.github.com>
2023-12-20 03:45:02 +0100

    Update theme-toggle.js

--- portfoliolekkipe_updated/theme-toggle.js
+++ portfoliolekkipe_updated/theme-toggle.js
@@ -1,22 +1,19 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const themeToggleButton = document.getElementById('dark-mode-toggle');
+    const themeToggleButton = document.getElementById('theme-toggle');
     const logoImage = document.getElementById('logo-image'); // L'élément img avec l'ID 'logo-image'
     const lightThemeLogoSrc = 'LEKKIPE titre.png'; // Le logo pour le thème clair
     const darkThemeLogoSrc = 'LEKKIPE titre dark.png'; // Le logo pour le thème sombre (changez ceci selon votre fichier)
-    const darkModeOnImageSrc = 'darkmode_on.png'; // Remplacez par le chemin de votre image "DarkMode ON"
-    const darkModeOffImageSrc = 'darkmode_off.png'; // Remplacez par le chemin de votre image "DarkMode OFF"
-    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-    
 
-    const applyDarkTheme = (apply) => {
-        const method = apply ? 'add' : 'remove';
-        document.body.classList[method]('dark-theme');
-        document.querySelector('header').classList[method]('dark-theme');
-        document.querySelector('main').classList[method]('dark-theme');
+    const currentTheme = localStorage.getItem('theme');
+
+    const applyDarkTheme = (isDark) => {
+        document.body.classList.toggle('dark-theme', isDark);
+        document.querySelector('header').classList.toggle('dark-theme', isDark);
+        document.querySelector('main').classList.toggle('dark-theme', isDark);
         document.querySelectorAll('article').forEach(article => {
-            article.classList[method]('dark-theme');
+            article.classList.toggle('dark-theme', isDark);
         });
-        themeToggleButton.src = apply ? darkModeOnImageSrc : darkModeOffImageSrc;
+        logoImage.src = isDark ? darkThemeLogoSrc : lightThemeLogoSrc;
     };
 
     if (currentTheme === 'dark-theme') {
@@ -26,6 +23,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     themeToggleButton.addEventListener('click', () => {
         const isDarkThemeApplied = document.body.classList.contains('dark-theme');
         applyDarkTheme(!isDarkThemeApplied);
-        localStorage.setItem('theme', isDarkThemeApplied ? 'light' : 'dark-theme');
+        localStorage.setItem('theme', !isDarkThemeApplied ? 'dark-theme' : 'light');
     });
 });
